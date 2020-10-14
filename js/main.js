3a6a1b87d5083b75be38203f7ef54f5c2f1c7685
KarrinaB <carinabogomolowa@yandex.ru>
2020-10-14 22:26:02 +0300

    create header&hero&mobile-menu

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var headerMenuMobile = document.querySelector(".header-menu__mobile")
+headerMenuMobile.addEventListener('click' , function () {
+document.querySelector(".header-menu").classList.toggle("header-menu--visible");
+});
